73ec231721af939666fb0386f7abe1009998e1db
Alexa <alexakearns@outlook.com>
2020-06-30 21:51:07 +0100

    Red: Sign up form takes details and redirects

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -5,4 +5,13 @@ RSpec.feature 'User Sign Up', type: :feature do
     visit '/'
     expect(page).to have_content('Sign Up')
   end
+
+  scenario 'user signs up using form and sees "Welcome"' do
+    visit '/'
+    fill_in 'user[name]', with: 'Alexa'
+    fill_in 'user[username]', with: 'alexaisthebest'
+    fill_in 'user[password]', with: '123456'
+    click_on 'Sign Up'
+    expect(page).to have_content('Welcome')
+  end
 end
