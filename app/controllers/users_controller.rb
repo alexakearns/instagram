b6f1c8f5d335eadfa545de730ff26cbffa7aa136
Alexa <alexakearns@outlook.com>
2020-07-01 12:24:45 +0100

    Add create with user_params and show to User contr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+  end
+
+  def show
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password_digest)
   end
 end
